28cdd378731703e4ef248bdb0c393c4332732f07
nairon.luiz <62431107+naironluiz@users.noreply.github.com>
2022-02-23 17:37:58 -0300

    porra

--- prototipocards/src/components/Cards.js
+++ prototipocards/src/components/Cards.js
@@ -1,15 +1,22 @@
+import styles from "./Cards.module.css";
+
 const Cards = (props) => {
   return (
-    <div className="cardContainer">
-      <button className="menuBtn"></button>
-      <div className="container">
-        <img src={props.img} alt="Foto de perfil" className="imagemPerfil" />
-        <h1 className="nome">{props.nome}</h1>
-        <h3 className="cargo">{props.cargo}</h3>
-        <p className="telLabel">Tel:</p>
-        <p className="tel">{props.tel}</p>
-        <p className="emailLabel">Email:</p>
-        <p className="email">{props.email}</p>
+    <div className={styles.cardContainer}>
+      <div className={styles.container}>
+        <button className={styles.menuBtn}>...</button>
+
+        <img
+          src={props.img}
+          alt="Foto de perfil"
+          className={styles.imagemPerfil}
+        />
+        <div className={styles.infoContainer}>
+          <h1 className={styles.nome}>{props.nome}</h1>
+          <h3 className={styles.cargo}>{props.cargo}</h3>
+          <p className={styles.tel}>{props.tel}</p>
+          <p className={styles.email}>{props.email}</p>
+        </div>
       </div>
     </div>
   );
--- prototipocards/src/index.js
+++ prototipocards/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "./index.css";
 
 //Declarar o root como variável
 const root = document.getElementById("root");
